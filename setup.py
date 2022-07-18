6d3a5927168383535a14db0b5c652a77636f8d82
rotohh <ianrotich95@gmail.com>
2022-07-18 13:46:35 +0300

    django with pandas

---
+++ setup.py
@@ -0,0 +1,48 @@
+import codecs
+from setuptools import setup, find_packages
+
+
+long_description = (
+    codecs.open('README.rst', 'r', 'utf-8').read() + '\n\n' +
+    codecs.open('CHANGES.rst', 'r', 'utf-8').read()
+)
+MAJOR = 0
+MINOR = 5
+MICRO = 1
+
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+setup(
+    name='django-pandas',
+    version=VERSION,
+    description='Tools for working with pydata.pandas in your Django projects',
+    long_description=long_description,
+    author='Christopher Clarke',
+    author_email='cclarke@chrisdev.com',
+    url='https://github.com/chrisdev/django-pandas/',
+    packages=find_packages(),
+    install_requires=[
+        'Django>=1.4.2',
+        'pandas>=0.14.1',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+    tests_require=[
+        "Django>=1.4.2",
+        "pandas==0.20.1",
+        "coverage>=4.0",
+                   ],
+    test_suite="runtests.runtests"
+
+)
